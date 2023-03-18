f8ad5c2bdaa099d83ec10eb3c3cebbd477d79e77
baizhi958216 <1475289190@qq.com>
2023-03-18 16:15:15 +0800

    Initialize

---
+++ src/router/index.ts
@@ -0,0 +1,25 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+      meta: {
+        title: '简历可视化'
+      }
+    }
+  ]
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.title) {
+    document.title = to.meta.title as string
+  }
+  next()
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<template>
+  <resume-review />
+</template>
+
+<script setup lang="ts">
+import ResumeReview from '@/components/ResumeReview.vue'
+</script>
+
+<style scoped></style>
